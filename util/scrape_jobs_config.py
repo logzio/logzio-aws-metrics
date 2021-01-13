7f771d50e2d5a03e4937b8e30b71681f8600be2b
yotamloe <yotam12341@gmail.com>
2021-01-13 15:53:30 +0200

    Add readme + update deafult values for docker compose

--- util/scrape_jobs_config.py
+++ util/scrape_jobs_config.py
@@ -1,7 +1,7 @@
 import os
 SCRAPE_INTERVAL = f'{os.environ["SCRAPE_INTERVAL"]}s'
 aws = {
-    'job_name': 'logzio-cloudwatch',
+    'job_name': 'logzio-cloudwatch-metrics',
     'scrape_interval': SCRAPE_INTERVAL,
     'scrape_timeout': SCRAPE_INTERVAL,
     'static_configs': [{
