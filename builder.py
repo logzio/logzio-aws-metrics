2f3d379e24ee66b0d358b781e8146f1a91b7e3d9
yotamloe <yotam12341@gmail.com>
2021-03-08 19:52:23 +0200

    Fix issues #5

--- builder.py
+++ builder.py
@@ -86,10 +86,11 @@ def _update_otel_config(token, region, p8s_name, otel_config):
         module_yaml['exporters']['prometheusremotewrite']['endpoint'] = _get_listener_url(region)
         module_yaml['exporters']['prometheusremotewrite']['headers'][
             'Authorization'] = f'Bearer {token}'
-        module_yaml['receivers']['prometheus']['config']['global']['external_labels'][
-            'p8s_logzio_name'] = p8s_name
+        # module_yaml['receivers']['prometheus']['config']['global']['external_labels'][
+        #     'p8s_logzio_name'] = p8s_name
         if scrape_jobs_config.aws not in module_yaml['receivers']['prometheus']['config']['scrape_configs']:
             module_yaml['receivers']['prometheus']['config']['scrape_configs'].append(scrape_jobs_config.aws)
+            module_yaml['receivers']['prometheus']['config']['scrape_configs']['labels']['p8s_logzio_name'] = p8s_name
         _dump_and_close_file(module_yaml, module_file)
         logger.info('Opentelemtry collector configuration ready')
 
--- util/scrape_jobs_config.py
+++ util/scrape_jobs_config.py
@@ -1,11 +1,12 @@
 import os
+
 SCRAPE_INTERVAL = f'{os.environ["SCRAPE_INTERVAL"]}s'
 aws = {
     'job_name': 'logzio-cloudwatch-metrics',
     'scrape_interval': SCRAPE_INTERVAL,
     'scrape_timeout': SCRAPE_INTERVAL,
     'static_configs': [{
-        'targets': ['cloudwatch-exporter:9106']
+        'targets': ['cloudwatch-exporter:9106'],
+        'labels': {'p8s_logzio_name': 'test'}
     }]
 }
-
